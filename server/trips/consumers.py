82f5e12e7be30b823d61ec56b0aa623ffb74ab57
Rafik Dev <rafikbdev@gmail.com>
2022-10-13 12:53:41 +0200

    Authenticating socket connections.

--- server/trips/consumers.py
+++ server/trips/consumers.py
@@ -4,11 +4,15 @@ from channels.generic.websocket import AsyncJsonWebsocketConsumer
 class TaxiConsumer(AsyncJsonWebsocketConsumer):
 
     async def connect(self):
-        await self.channel_layer.group_add(
-            group='test',
-            channel=self.channel_name
-        )
-        await self.accept()
+        user = self.scope['user']
+        if user.is_anonymous:
+            await self.close()
+        else:
+            await self.channel_layer.group_add(
+                group='test',
+                channel=self.channel_name
+            )
+            await self.accept()
 
     async def disconnect(self, code):
         await self.channel_layer.group_add(
--- server/trips/tests/test_websocket.py
+++ server/trips/tests/test_websocket.py
@@ -1,6 +1,10 @@
 import pytest
+from channels.db import database_sync_to_async
 from channels.layers import get_channel_layer
 from channels.testing import WebsocketCommunicator
+
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import AccessToken
 
 from taxi.routing import application
 
@@ -10,14 +14,29 @@ TEST_CHANNEL_LAYERS = {
     }
 }
 
+@database_sync_to_async
+def create_user(username, password):
+    user = get_user_model().objects.create_user(
+        username=username,
+        password=password
+    )
+    access = AccessToken.for_user(user)
+    return user, access
+
 
 @pytest.mark.asyncio
+@pytest.mark.django_db(transaction=True)
 class TestWebSocket:
     async def test_can_connect_to_server(self, settings):
         settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        _, access = await create_user(
+            'test.user@example.com',
+            'pAssw0rd'
+        )
+
         communicator = WebsocketCommunicator(
             application=application,
-            path='/taxi/'
+            path=f'/taxi/?token={access}'
         )
         connected, _ = await communicator.connect()
         assert connected is True
@@ -26,9 +45,14 @@ class TestWebSocket:
     async def test_can_send_and_receive_messages(self, settings):
         """Test if user can send message in empty room"""
         settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        _, access = await create_user(
+            'test.user@example.com',
+            'pAssw0rd'
+        )
+
         communicator = WebsocketCommunicator(
             application=application,
-            path='/taxi/'
+            path=f'/taxi/?token={access}'
         )
         await communicator.connect()
         message = {
@@ -44,9 +68,14 @@ class TestWebSocket:
     async def test_can_send_and_receive_broadcast_message(self, settings):
         """Test if user can send message in room with other people"""
         settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        _, access = await create_user(
+            'test.user@example.com',
+            'pAssw0rd'
+        )
+
         communicator = WebsocketCommunicator(
             application=application,
-            path='/taxi/'
+            path=f'/taxi/?token={access}'
         )
         await communicator.connect()
         message = {
@@ -58,4 +87,15 @@ class TestWebSocket:
 
         response = await communicator.receive_json_from()
         assert response == message
+        await communicator.disconnect()
+
+    async def test_cannot_connect_to_socket(self, settings):
+        """Test if handshake request fails if no valid access token is included."""
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        communicator = WebsocketCommunicator(
+            application=application,
+            path='/taxi/'
+        )
+        connected, _ = await communicator.connect()
+        assert connected is False
         await communicator.disconnect()
