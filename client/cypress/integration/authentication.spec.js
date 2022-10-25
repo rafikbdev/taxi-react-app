2993ec595f4b022bfecce9014583de9e77437a52
Rafik Dev <rafikbdev@gmail.com>
2022-10-25 13:45:20 +0200

    Refactor use addUser in client tests.

--- client/cypress/integration/authentication.spec.js
+++ client/cypress/integration/authentication.spec.js
@@ -1,38 +1,24 @@
 const faker = require('faker');
 
-const randomEmail = faker.internet.email();
+const email = faker.internet.email();
+const firstName = faker.name.firstName();
+const lastName = faker.name.lastName();
 
 const logIn = () => {
-    const { username, password } = Cypress.env('credentials');
-
     // Capture HTTP requests.
     cy.intercept('POST', 'log_in').as('logIn');
 
     // Log into the app.
     cy.visit('/#/log-in');
-    cy.get('input#username').type(randomEmail);
-    cy.get('input#password').type(password, { log: false });
+    cy.get('input#username').type(email);
+    cy.get('input#password').type('pAssw0rd', { log: false });
     cy.get('button').contains('Log in').click();
     cy.wait('@logIn');
 };
 
 describe('Authentication', function () {
     it('Can sign up.', function () {
-        cy.intercept('POST', 'sign_up').as('signUp');
-
-        cy.visit('/#/sign-up');
-        cy.get('input#username').type(randomEmail);
-        cy.get('input#firstName').type('Gary');
-        cy.get('input#lastName').type('Cole');
-        cy.get('input#password').type('pAssw0rd', { log: false });
-        cy.get('select#group').select('driver');
-
-        // Handle file upload
-        cy.get('input#photo').attachFile('images/photo.jpg');
-
-        cy.get('button').contains('Sign up').click();
-        cy.wait('@signUp');
-        cy.hash().should('eq', '#/log-in');
+        cy.addUser(email, firstName, lastName, 'rider');
     });
 
     it('Cannot visit the sign up page when logged in.', function () {
@@ -44,7 +30,7 @@ describe('Authentication', function () {
     it('Can log out.', function () {
         logIn();
         cy.get('button').contains('Log out').click().should(() => {
-        expect(window.localStorage.getItem('taxi.auth')).to.be.null;
+            expect(window.localStorage.getItem('taxi.auth')).to.be.null;
         });
         cy.get('button').contains('Log out').should('not.exist');
     });
@@ -59,7 +45,7 @@ describe('Authentication', function () {
         }
         }).as('signUp');
         cy.visit('/#/sign-up');
-        cy.get('input#username').type(randomEmail);
+        cy.get('input#username').type(email);
         cy.get('input#firstName').type('Gary');
         cy.get('input#lastName').type('Cole');
         cy.get('input#password').type('pAssw0rd', { log: false });
@@ -70,7 +56,7 @@ describe('Authentication', function () {
         cy.get('button').contains('Sign up').click();
         cy.wait('@signUp');
         cy.get('div.invalid-feedback').contains(
-        'A user with that username already exists'
+            'A user with that username already exists'
         );
         cy.hash().should('eq', '#/sign-up');
     });
@@ -94,7 +80,6 @@ describe('Authentication', function () {
     });
 
     it('Shows an alert on login error.', function () {
-        const { username, password } = Cypress.env('credentials');
         cy.intercept('POST', 'log_in', {
         statusCode: 400,
         body: {
@@ -105,8 +90,8 @@ describe('Authentication', function () {
         }
         }).as('logIn');
         cy.visit('/#/log-in');
-        cy.get('input#username').type(randomEmail);
-        cy.get('input#password').type(password, { log: false });
+        cy.get('input#username').type(email);
+        cy.get('input#password').type('pAssw0rd', { log: false });
         cy.get('button').contains('Log in').click();
         cy.wait('@logIn');
         cy.get('div.alert').contains(
